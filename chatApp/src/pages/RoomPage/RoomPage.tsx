0c1ddfb10257860808ea06ed6bd80e6ea6bed8a0
Тимофей Атласов <Arctic_Stranger@Noutbuk-Saryal-Timofej.local>
2026-06-08 20:22:09 +0900

    modal + input in roompage

--- chatApp/src/pages/RoomPage/RoomPage.tsx
+++ chatApp/src/pages/RoomPage/RoomPage.tsx
@@ -1,13 +1,44 @@
-import { DndContext } from '@dnd-kit/core';
+// import Draggable from 'react-draggable';
+import { useState } from 'react';
+import './RoomPage.css';
+import Modal from './modal.tsx';
+import { Input } from 'antd';
+import type { InputNumberProps } from 'antd';
+import { Flex, InputNumber } from 'antd';
 
+const onChange: InputNumberProps['onChange'] = (value) => {
+  console.log('changed', value);
+};
+
+const sharedProps = {
+  mode: 'spinner' as const,
+  min: 2,
+  max: 4,
+  defaultValue: 2,
+  onChange,
+  style: { width: '40%', height: '4vh', fontSize: '1.5rem' },
+};
 export function RoomPage() {
+  // const nodeRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
   return (
+    // <Draggable nodeRef={nodeRef}>
     <>
-      <DndContext>
-        <h1>header RoomPage</h1>
-        <p>smth</p>
-        {/* <div className="room-card" ref={setNodeRef} {...listeners} {...attributes}></div> */}
-      </DndContext>
+      <button className="room-elem" onClick={() => setIsOpen(true)}>
+        Drag me
+      </button>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <p>some text of modal</p>
+      </Modal>
+      <h2>Создайте название комнаты:</h2>
+      <Input placeholder="test" className="input-name-room" />
+      <br />
+      <h2>Макс. кол-во участников:</h2>
+      <Flex vertical gap="medium">
+        <InputNumber {...sharedProps} placeholder="Outlined" />
+      </Flex>
     </>
+    // </Draggable>
   );
 }
