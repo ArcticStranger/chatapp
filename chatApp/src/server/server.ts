9ab1597e5ab085d7e8b35c40764cb3964e6b9fab
Тимофей Атласов <Arctic_Stranger@Noutbuk-Saryal-Timofej.local>
2026-06-12 01:29:45 +0900

    huh

--- chatApp/src/server/server.ts
+++ chatApp/src/server/server.ts
@@ -32,15 +32,31 @@ io.on('connection', (socket: Socket) => {
   console.log(`Пользователь подключился: ${socket.id}`);
 
   // Юзер сообщает, в какую комнату хочет войти
-  socket.on('join-room', (roomId: string) => {
+  socket.on('join-room', async (roomId: string) => {
     socket.join(roomId);
+
+    const { data, error } = await supabase.from('messages').select();
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    console.log(data);
     console.log(`Пользователь ${socket.id} вошел в комнату: ${roomId}`);
   });
 
   // Пересылка сообщений между участниками конкретной комнаты
-  socket.on('send-message', (data: MessageData) => {
+  socket.on('send-message', async (data: MessageData) => {
     // Отправляем сообщение всем в комнате, кроме самого отправителя
+
+    const { error } = await supabase
+      .from('messages')
+      .insert({ room_id: data.roomId, text: data.text, sender: data.sender });
     socket.to(data.roomId).emit('receive-message', data);
+    if (error) {
+      console.log(error);
+      return;
+    }
   });
 
   socket.on('disconnect', () => {
